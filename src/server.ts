9678606436c855d91f99cbff8184dbf3ab1ca5fd
Leo <leooo0s2@icloud.com>
2026-01-13 11:48:53 -0300

    Insert com Knex Raw

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,8 @@ app.use(express.json());
 app.post("/courses", async (request: Request, response: Response) => {
   const { name } = request.body;
 
-  await knex("courses").insert({
-    name,
-  });
+  // await knex("courses").insert({  name, });
+  await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
 
   response.status(201).json();
 });
