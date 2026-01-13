695928ddc4d360568fbd4f3f38e5644a5ee61b04
Leo <leooo0s2@icloud.com>
2026-01-13 11:57:57 -0300

    Update

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,23 @@ app.post("/courses", async (request: Request, response: Response) => {
   await knex("courses").insert({ name });
   // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
 
-  response.status(201).json();
+  return response.status(201).json();
 });
 
 app.get("/courses", async (request: Request, response: Response) => {
   // const courses = await knex.raw("SELECT * FROM courses");
   const courses = await knex("courses").select("").orderBy("name");
 
-  response.json(courses);
+  return response.json(courses);
+});
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  await knex("courses").update({ name }).where({ id });
+
+  return response.json();
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
